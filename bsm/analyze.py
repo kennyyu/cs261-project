23bd0b3bb9e926b7c8c284c930192adcf3753788
R.J. Aquino <rjaquino@college.harvard.edu>
2013-12-09 22:03:56 -0500

    Added basic Parzen functions

--- bsm/analyze.py
+++ bsm/analyze.py
@@ -7,6 +7,13 @@ import sys
 import bsm
 import histogram
 
+# functions to run on the graph (and aggregate results by node type)
+FUNCTIONS = {
+        "opsahl": histogram.centrality_opsahl,
+        "indegree": histogram.centrality_in_degree,
+        "ancestor": histogram.centrality_ancestor
+        }
+
 def process_counts(g):
     # count nodes by type
     proc_counts = defaultdict(int)
@@ -24,15 +31,13 @@ def edge_counts(g):
     """
     Returns a dictionary of process name to a list of edge type counts
     """
+    # XXX TODO
     return {}
 
-# functions to run on the graph (and aggregate results by node type)
-FUNCTIONS = {
-        "opsahl": histogram.centrality_opsahl,
-        "indegree": histogram.centrality_in_degree,
-        "ancestor": histogram.centrality_ancestor
-        }
 def get_values(g, functions):
+    """
+    Returns a dictionary of names to functions to list of values
+    """
     metrics = {}
 
     # go through each metric
@@ -53,6 +58,25 @@ def get_values(g, functions):
                 # add this particular value to the metrics dict
                 metrics[cmd][f].append(values[node_num])
     return metrics
+
+# gaussian taken from http://stackoverflow.com/questions/14873203/plotting-of-1-dimensional-gaussian-distribution-function
+def gaussian(x, mu, sig):
+    return np.exp(-np.power(x - mu, 2.) / 2 * np.power(sig, 2.)) * 1./(2*math.PI*sig)
+def parzen_estimate(x, vals):
+    std = np.std(vals)
+    return 1./len(vals) * sum(gaussian(x-val, x, std) for val in vals)
+
+def parzen_is_expected(x, vals, psi):
+    # get log liklihood of x
+    ll_x = math.log(parzen_estimate(x, vals))
+
+    # count y in vals for ll(y) < ll(x)
+    count_y = sum(1 if math.log(parzen_estimate(y, vals)) < ll_x else 0 for y in vals)
+
+    # if this probability is greater than psi, accept
+    return float(count_y) / len(vals) > psi
+
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
